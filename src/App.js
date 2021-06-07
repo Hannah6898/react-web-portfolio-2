1703d4100923c23477c2aad82f9797a1c8a48f85
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-07 13:23:08 +0100

    Refactored code, Added CSS grid layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="nav">Nav</div>
+        <div className="home">Home</div>
+        <div className="about">About Me</div>
+        <div className="education">Education</div>
+        <div className="tech">Tech stack</div>
+        <div className="projects">Projects</div>
+        <div className="contact">Contact Me</div>
+      </div>
     </div>
   );
 }
-
-export default App;
