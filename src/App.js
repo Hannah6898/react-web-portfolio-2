6dcef4ce57e23286ece942d29e95cb603d38f0c9
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-13 11:11:49 +0100

    Working on background

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Education from "./Education/Education";
 import Tech from "./Tech/Tech";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
+import ParticleBackground from "./ParticleBackground";
 
 export default function App() {
   return (
     <div className="App">
+      <div>
+        <ParticleBackground />
+      </div>
+
       <div className="wrapper">
         <Nav />
         <Home />
