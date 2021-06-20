642d19381fa7ae348be981131f2edf4c7e875342
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-20 12:47:08 +0100

    Changed text in About component and styled button

--- src/About/About.js
+++ src/About/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -7,6 +8,15 @@ const useStyles = makeStyles(() => ({
   },
   header: {
     textAlign: "center",
+  },
+  pink: {
+    color: "#da0463",
+  },
+  button: {
+    display: "block",
+    margin: "0 auto",
+    backgroundColor: "#da0463",
+    color: "white",
   },
 }));
 
@@ -17,16 +27,32 @@ export default function About() {
       <div className={classes.root}>
         <h1 className={classes.header}>About Me</h1>
         <p>
-          I am a Software Developer who through enthusiasm and self-motivation
-          has learnt to code whilst working full time. I am competent in several
-          coding languages including React and JavaScript, and am continuing to
-          further my learning. I have proven my skills through developing
-          projects such as a React weather app which you can find linked below.
-          Currently I’m seeking a long-term position where I can utilise my
-          transferable skills such as problem solving and data analysis; and
-          develop my skills.
+          I’m Hannah, a <span className={classes.pink}>Software Developer</span>{" "}
+          based in Kent currently looking for my first junior role.
         </p>
-        <button>CV</button>
+        <p>
+          I began learning to code at the beginning of this year and found the
+          process of learning new technologies and creating solutions to
+          difficult problems both exciting and rewarding.
+        </p>
+        <p>
+          I have specialised thus far in front end technologies like React and
+          am eager to deepen my expertise in this area; with my evenings
+          currently spent deepening my JavaScript knowledge and creating React
+          projects.
+        </p>
+        <p>
+          My long term goal is be able to efficiently turn great ideas into
+          functioning products thorough absorbing as much as I can from those my
+          senior to become the best developer I can be.
+        </p>
+        <p>
+          In my spare time I like to roller skate, watch TV and movies, and I’m
+          currently getting into Formula 1.
+        </p>
+        <Button variant="contained" className={classes.button}>
+          CV
+        </Button>
       </div>
     </div>
   );
