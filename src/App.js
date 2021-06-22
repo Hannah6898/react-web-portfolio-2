454f3a50ad8ca7cf77ca29183fbf4343bdc71d6c
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-22 19:24:37 +0100

    Removed particle.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ import Tech from "./Tech/Tech";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
 import Footer from "./Footer/Footer";
-import ParticleBackground from "./ParticleBackground";
 
 export default function App() {
   return (
     <div className="App">
-      <ParticleBackground />
       <div className="wrapper">
         <Nav />
         <Home />
