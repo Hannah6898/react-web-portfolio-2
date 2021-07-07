709a97fedacc78ab515fcd0658f09041e012c394
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-07-07 19:13:11 +0100

    Added margin to div's in Education component

--- src/Education/Education.js
+++ src/Education/Education.js
@@ -15,6 +15,9 @@ const useStyles = makeStyles(() => ({
     paddingLeft: "5px",
     fontSize: "23px",
   },
+  certificate: {
+    margin: "10px 0 20px 0",
+  },
 }));
 
 export default function Education() {
@@ -26,37 +29,43 @@ export default function Education() {
           Education
         </Typography>
         <Typography variant="h6">Certificates</Typography>
-        <Typography variant="body1" gutterBottom>
-          SheCodes React | Issued Apr 2021
-          <Link
-            href="https://www.shecodes.io/certificates/bfa3e58394a09554e62af5db21128917"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GoLink className={classes.pink} color="primary" />
-          </Link>
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          {" "}
-          SheCodes Plus | Issued Mar 2021
-          <Link
-            href="https://www.shecodes.io/certificates/d5e8d27ccd85b518a996a83aa132d98b"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GoLink className={classes.pink} color="primary" />
-          </Link>
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          SheCodes Basics | Issued Feb 2021{" "}
-          <Link
-            href="https://www.shecodes.io/certificates/02ed5586804bd6c78fc1b9d58bc8ea47"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <GoLink className={classes.pink} color="primary" />
-          </Link>
-        </Typography>
+        <div className={classes.certificate}>
+          <Typography variant="body1" gutterBottom>
+            SheCodes React | Issued Apr 2021
+            <Link
+              href="https://www.shecodes.io/certificates/bfa3e58394a09554e62af5db21128917"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GoLink className={classes.pink} color="primary" />
+            </Link>
+          </Typography>
+        </div>
+        <div className={classes.certificate}>
+          <Typography variant="body1" gutterBottom>
+            {" "}
+            SheCodes Plus | Issued Mar 2021
+            <Link
+              href="https://www.shecodes.io/certificates/d5e8d27ccd85b518a996a83aa132d98b"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GoLink className={classes.pink} color="primary" />
+            </Link>
+          </Typography>
+        </div>
+        <div className={classes.certificate}>
+          <Typography variant="body1" gutterBottom>
+            SheCodes Basics | Issued Feb 2021{" "}
+            <Link
+              href="https://www.shecodes.io/certificates/02ed5586804bd6c78fc1b9d58bc8ea47"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GoLink className={classes.pink} color="primary" />
+            </Link>
+          </Typography>
+        </div>
       </div>
     </div>
   );
