635a223d970a92275219bd55db3b9200c5a6733a
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-07-11 13:19:30 +0100

    Refactored code - Project component images

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -26,6 +26,7 @@ const useStyles = makeStyles((theme) => ({
     color: "black",
   },
   logo: {
+    aspectRatio: "attr(50) / attr(48)",
     width: "50px",
     "&:hover": {
       cursor: "pointer",
