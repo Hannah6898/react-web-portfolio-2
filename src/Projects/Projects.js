6f4506b06f33d67659ea8f32ceed7b2cc10d90e1
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-07-19 14:26:17 +0100

    Refactored Tech and Projects components

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -17,15 +17,21 @@ import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    textAlign: "center",
+    paddingBottom: "20px",
+  },
+  projectAccordion: {
     width: "90%",
     margin: "0 auto",
+  },
+  block: {
+    display: "block",
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     flexBasis: "33.33%",
     flexShrink: 0,
   },
-
   accordion: {
     margin: "10px",
     backgroundColor: "#da0463",
@@ -33,20 +39,8 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "10px",
     color: "white",
   },
-
-  expand: {
+  expandArrow: {
     color: "white",
-  },
-
-  description: {
-    textAlign: "left",
-    paddingLeft: "20px",
-  },
-  techIcon: {
-    fontSize: "30px",
-  },
-  block: {
-    display: "block",
   },
   link: {
     fontSize: "30px",
@@ -65,254 +59,254 @@ export default function Projects() {
 
   return (
     <div className="Projects" id="projects">
-      <Typography variant="h4" gutterBottom>
-        Projects
-      </Typography>
       <div className={classes.root}>
-        <Accordion
-          expanded={expanded === "panel1"}
-          onChange={handleChange("panel1")}
-          className={classes.accordion}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon className={classes.expand} />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
+        <Typography variant="h4" gutterBottom>
+          Projects
+        </Typography>
+        <div className={classes.projectAccordion}>
+          <Accordion
+            expanded={expanded === "panel1"}
+            onChange={handleChange("panel1")}
+            className={classes.accordion}
           >
-            <Typography className={classes.heading}>Weather App</Typography>
-            <Typography className={classes.secondaryHeading}>
-              HTML | CSS | JS | React | API | Bootstrap | Netlify
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className={classes.block}>
-            <div>
-              <img
-                src={WeatherApp}
-                className="weatherapp"
-                alt="WeatherApp"
-              ></img>
-            </div>
-            <Typography variant="body1" className={classes.description}>
-              A weather application which dynamically displays the present
-              weather of the users desired location and current location, using
-              Open Weather Map API. Through this project I became skilled in
-              using Bootstrap, API's, real- life development workflow, hosting
-              and JavaScript.
-            </Typography>
-
-            <Link
-              href="https://fervent-galileo-89d4fe.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="See Live"
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon className={classes.expandArrow} />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
             >
-              {" "}
-              <GoLink className={classes.link} />
-            </Link>
-            <Link
-              href="https://github.com/Hannah6898/react-weather-app"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Github"
+              <Typography className={classes.heading}>Weather App</Typography>
+              <Typography>
+                HTML | CSS | JS | React | API | Bootstrap | Netlify
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.block}>
+              <div>
+                <img
+                  src={WeatherApp}
+                  className="portrait-img"
+                  alt="WeatherApp"
+                ></img>
+              </div>
+              <Typography variant="body1">
+                A weather application which dynamically displays the present
+                weather of the users desired location and current location,
+                using Open Weather Map API. Through this project I became
+                skilled in using Bootstrap, API's, real- life development
+                workflow, hosting and JavaScript.
+              </Typography>
+
+              <Link
+                href="https://fervent-galileo-89d4fe.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="See Live"
+              >
+                {" "}
+                <GoLink className={classes.link} />
+              </Link>
+              <Link
+                href="https://github.com/Hannah6898/react-weather-app"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Github"
+              >
+                {" "}
+                <AiFillGithub className={classes.link} />
+              </Link>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel2"}
+            onChange={handleChange("panel2")}
+            className={classes.accordion}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon className={classes.expandArrow} />}
+              aria-controls="panel2bh-content"
+              id="panel2bh-header"
             >
-              {" "}
-              <AiFillGithub className={classes.link} />
-            </Link>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel2"}
-          onChange={handleChange("panel2")}
-          className={classes.accordion}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon className={classes.expand} />}
-            aria-controls="panel2bh-content"
-            id="panel2bh-header"
+              <Typography className={classes.heading}>Dictionary</Typography>
+              <Typography>HTML | CSS | JS | React | API | Netlify</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.block}>
+              <div>
+                <img
+                  src={Dictionary}
+                  className="landscape-img"
+                  alt="Dictionary"
+                ></img>
+              </div>
+
+              <div>
+                <Typography>
+                  I've used my knowledge of React to build a working dictionary
+                  app incorporating coding best practices, along with the most
+                  popular tools and technologies.
+                </Typography>
+
+                <Link
+                  href="https://gallant-boyd-0ddfe2.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="See Live"
+                >
+                  {" "}
+                  <GoLink className={classes.link} />
+                </Link>
+                <Link
+                  href="https://github.com/Hannah6898/react-dictionary"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Github"
+                >
+                  {" "}
+                  <AiFillGithub className={classes.link} />
+                </Link>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel3"}
+            onChange={handleChange("panel3")}
+            className={classes.accordion}
           >
-            <Typography className={classes.heading}>Dictionary</Typography>
-            <Typography className={classes.secondaryHeading}>
-              HTML | CSS | JS | React | API | Netlify
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className={classes.block}>
-            <div>
-              <img
-                src={Dictionary}
-                className="dictionary"
-                alt="Dictionary"
-              ></img>
-            </div>
-
-            <div>
-              <Typography className={classes.description}>
-                I've used my knowledge of React to build a working dictionary
-                app incorporating coding best practices, along with the most
-                popular tools and technologies.
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon className={classes.expandArrow} />}
+              aria-controls="panel3bh-content"
+              id="panel3bh-header"
+            >
+              <Typography className={classes.heading}>Portfolio</Typography>
+              <Typography>
+                HTML | CSS | JS | React | Material UI | Netlify
               </Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.block}>
+              <div>
+                <img
+                  src={Portfolio}
+                  className="landscape-img"
+                  alt="portfolio"
+                ></img>
+              </div>
+              <div>
+                <Typography>
+                  Made my personal portfolio using React and Material UI.
+                  Furthermore, I used smooth scroll to create seamless
+                  navigation between components.
+                </Typography>
 
-              <Link
-                href="https://gallant-boyd-0ddfe2.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="See Live"
-              >
-                {" "}
-                <GoLink className={classes.link} />
-              </Link>
-              <Link
-                href="https://github.com/Hannah6898/react-dictionary"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Github"
-              >
-                {" "}
-                <AiFillGithub className={classes.link} />
-              </Link>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel3"}
-          onChange={handleChange("panel3")}
-          className={classes.accordion}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon className={classes.expand} />}
-            aria-controls="panel3bh-content"
-            id="panel3bh-header"
+                <Link
+                  href="https://www.hannahosibodu.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="See Live"
+                >
+                  {" "}
+                  <GoLink className={classes.link} />
+                </Link>
+                <Link
+                  href="https://github.com/Hannah6898/react-web-portfolio-2"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Github"
+                >
+                  {" "}
+                  <AiFillGithub className={classes.link} />
+                </Link>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel4"}
+            onChange={handleChange("panel4")}
+            className={classes.accordion}
           >
-            <Typography className={classes.heading}>Portfolio</Typography>
-            <Typography className={classes.secondaryHeading}>
-              HTML | CSS | JS | React | Material UI | Netlify
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className={classes.block}>
-            <div>
-              <img src={Portfolio} className="dictionary" alt="portfolio"></img>
-            </div>
-            <div>
-              <Typography className={classes.description}>
-                Made my personal portfolio using React and Material UI.
-                Furthermore, I used smooth scroll to create seamless navigation
-                between components.
-              </Typography>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon className={classes.expandArrow} />}
+              aria-controls="panel4bh-content"
+              id="panel4bh-header"
+            >
+              <Typography className={classes.heading}>Youtube Clone</Typography>
+              <Typography>HTML | CSS | JS | Netlify</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.block}>
+              <div>
+                <img src={Youtube} className="landscape" alt="Youtube"></img>
+              </div>
+              <div>
+                <Typography>
+                  Developed a site clone of the YouTube homepage using HTML, CSS
+                  and JavaScript.
+                </Typography>
 
-              <Link
-                href="https://www.hannahosibodu.com/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="See Live"
-              >
-                {" "}
-                <GoLink className={classes.link} />
-              </Link>
-              <Link
-                href="https://github.com/Hannah6898/react-web-portfolio-2"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Github"
-              >
-                {" "}
-                <AiFillGithub className={classes.link} />
-              </Link>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel4"}
-          onChange={handleChange("panel4")}
-          className={classes.accordion}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon className={classes.expand} />}
-            aria-controls="panel4bh-content"
-            id="panel4bh-header"
+                <Link
+                  href="https://elegant-sinoussi-498e6f.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="See Live"
+                >
+                  {" "}
+                  <GoLink className={classes.link} />
+                </Link>
+                <Link
+                  href="https://github.com/Hannah6898/Youtube-Clone"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Github"
+                >
+                  {" "}
+                  <AiFillGithub className={classes.link} />
+                </Link>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion
+            expanded={expanded === "panel5"}
+            onChange={handleChange("panel5")}
+            className={classes.accordion}
           >
-            <Typography className={classes.heading}>Youtube Clone</Typography>
-            <Typography className={classes.secondaryHeading}>
-              HTML | CSS | JS | Netlify
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className={classes.block}>
-            <div>
-              <img src={Youtube} className="dictionary" alt="Youtube"></img>
-            </div>
-            <div>
-              <Typography className={classes.description}>
-                Developed a site clone of the YouTube homepage using HTML, CSS
-                and JavaScript.
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon className={classes.expandArrow} />}
+              aria-controls="panel5bh-content"
+              id="panel5bh-header"
+            >
+              <Typography className={classes.heading}>
+                JavaScript Drum Kit
               </Typography>
+              <Typography>HTML | CSS | JS | Netlify</Typography>
+            </AccordionSummary>
+            <AccordionDetails className={classes.block}>
+              <div>
+                <img src={DrumKit} className="landscape" alt="DrumKit"></img>
+              </div>
+              <div>
+                <Typography>
+                  Created a JavaScript Drum kit using Keyboard events and styled
+                  using CSS grid.
+                </Typography>
 
-              <Link
-                href="https://elegant-sinoussi-498e6f.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="See Live"
-              >
-                {" "}
-                <GoLink className={classes.link} />
-              </Link>
-              <Link
-                href="https://github.com/Hannah6898/Youtube-Clone"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Github"
-              >
-                {" "}
-                <AiFillGithub className={classes.link} />
-              </Link>
-            </div>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel5"}
-          onChange={handleChange("panel5")}
-          className={classes.accordion}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon className={classes.expand} />}
-            aria-controls="panel5bh-content"
-            id="panel5bh-header"
-          >
-            <Typography className={classes.heading}>
-              JavaScript Drum Kit
-            </Typography>
-            <Typography className={classes.secondaryHeading}>
-              HTML | CSS | JS | Netlify
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails className={classes.block}>
-            <div>
-              <img src={DrumKit} className="dictionary" alt="DrumKit"></img>
-            </div>
-            <div>
-              <Typography className={classes.description}>
-                Created a JavaScript Drum kit using Keyboard events and styled
-                using CSS grid.
-              </Typography>
-
-              <Link
-                href="https://dreamy-kalam-3b5e13.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="See Live"
-              >
-                {" "}
-                <GoLink className={classes.link} />
-              </Link>
-              <Link
-                href="https://github.com/Hannah6898/JavaScript-Drum-Kit"
-                target="_blank"
-                rel="noreferrer"
-                aria-label="Github"
-              >
-                {" "}
-                <AiFillGithub className={classes.link} />
-              </Link>
-            </div>
-          </AccordionDetails>
-        </Accordion>
+                <Link
+                  href="https://dreamy-kalam-3b5e13.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="See Live"
+                >
+                  {" "}
+                  <GoLink className={classes.link} />
+                </Link>
+                <Link
+                  href="https://github.com/Hannah6898/JavaScript-Drum-Kit"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Github"
+                >
+                  {" "}
+                  <AiFillGithub className={classes.link} />
+                </Link>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
       </div>
     </div>
   );
