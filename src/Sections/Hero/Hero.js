456be06847ab31a88b6a6e7f8cdb205affb89bdc
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-09-22 09:47:51 +0100

    Added content to Hero.js component

--- src/Sections/Hero/Hero.js
+++ src/Sections/Hero/Hero.js
@@ -1,17 +1,24 @@
 import React from "react";
+import classes from "./Hero.module.css";
 import HeroImg from "./Hero-img.png";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Aspiring Junior Developer seeking role with React</h1>
-      <h2>
-        Skilled in React and JavaScript, currently looking for my first role
-        within the tech industry{" "}
-      </h2>
-      <button>Let's Chat</button>
-      <img src={HeroImg}></img>
-    </div>
+    <section>
+      <div className={classes.container}>
+        <div className={classes.textContainer}>
+          <h1 className={classes.heading}>
+            Aspiring Junior Developer seeking role with React
+          </h1>
+          <h2 className={classes.subheading}>
+            Skilled in React and JavaScript, currently looking for my first role
+            within the tech industry{" "}
+          </h2>
+          <button className={classes.btn}>Let's Chat</button>
+        </div>
+        <img src={HeroImg} alt="Hero images" className="heroImg"></img>
+      </div>
+    </section>
   );
 };
 
