fc47293c7a6f9675453844c8b4fa5ed07dae1533
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-09-23 11:52:14 +0100

    Added content for Contact component

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -1,5 +1,10 @@
 import React from "react";
 import classes from "./Contact.module.css";
+import {
+  AiFillGithub,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 
 const Contact = () => {
   return (
@@ -9,22 +14,38 @@ const Contact = () => {
           <h3>Connect with me!</h3>
         </div>
         <form>
+          <input type="hidden" name="_subject" value="New submission!" />
           <div className={classes.input}>
-            <label>Name</label>
-            <input type="text"></input>
+            <label form="name">Name</label>
+            <input type="text" required className={classes.singleLine}></input>
           </div>
           <div className={classes.input}>
-            <label>Email</label>
-            <input type="email"></input>
+            <label form="email">Email</label>
+            <input type="email" required className={classes.singleLine}></input>
           </div>
           <div className={classes.input}>
-            <label>Message</label>
-            <input type="text"></input>
+            <label for="message">Message</label>
+            <textarea rows="5" col="30" name="message" required></textarea>
           </div>
           <button type="submit" className={classes.btn}>
             {" "}
             Send Message
           </button>
+          <span className={classes.icons}>
+            <a href="https://github.com/Hannah6898" target="_blank">
+              {" "}
+              <AiFillGithub />{" "}
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hannah-osibodu-63b524152/"
+              target="_blank"
+            >
+              <AiFillLinkedin />{" "}
+            </a>
+            <a href="https://twitter.com/hanc0des" target="_blank">
+              <AiFillTwitterCircle />
+            </a>
+          </span>
         </form>
       </div>
     </section>
